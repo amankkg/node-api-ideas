ef4eda6e5b40d3f2b44c8805ed3fdddf1e8309c5
Aman Kubanychbek <amankubanychbek@gmail.com>
2018-05-27 16:44:38 +0300

    add basic koa app

---
+++ index.mjs
@@ -0,0 +1,22 @@
+import http from 'http'
+
+import Koa from 'koa'
+import router from 'koa-route'
+
+import { add } from './add'
+
+const app = new Koa()
+
+app.use(
+  router.get('/', async function(ctx, next) {
+    ctx.body = `1 + 2 = ${add(1, 2)}`
+
+    await next()
+  }),
+)
+
+http.createServer({}, app.callback()).listen(3000, err => {
+  if (err) throw new Error(err)
+
+  console.log('Listening on port 3000.')
+})
